a1ac4f947096d48fbdd86f1597ba5b57671a4ed9
jannu99 <jannussi99@gmail.com>
2026-06-01 16:13:09 +0200

    minor fix

--- 3_generate_simulation_data/step3_sd.sh
+++ 3_generate_simulation_data/step3_sd.sh
@@ -17,13 +17,13 @@ set -e
 
 SCRIPTS=(
     "3_generate_simulation_data/3B_sd_transform_parked_vehicles_to_carla.py"
-    "3_generate_simulation_data/3C_setup_carla.py"
+    "3_generate_simulation_data/3C_sd_setup_carla.py"
     "3_generate_simulation_data/3F_sd_generate_carla_scenario.py"
 )
 
 for SCRIPT in "${SCRIPTS[@]}"; do
     echo "Running Script $SCRIPT"
-    if [ "$SCRIPT" != "3_generate_simulation_data/3C_setup_carla.py" ]; then
+    if [ "$SCRIPT" != "3_generate_simulation_data/3C_sd_setup_carla.py" ]; then
         python3 "$SCRIPT"
         if [ $? -ne 0 ]; then
             echo "Error in $SCRIPT. Aborting."
